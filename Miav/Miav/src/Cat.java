f5d5b577e42538e93e27ce51428c98b343ec74e4
FHallengreen <fred70p9@stud.kea.dk>
2022-04-19 16:02:14 +0200

    Reupload

--- Miav/Miav/src/Cat.java
+++ Miav/Miav/src/Cat.java
@@ -3,35 +3,31 @@ public class Cat {
   private int width;
   private int weight;
   private boolean isBaby; //if false = adult.
-  boolean isSneaky;
 
-  public Cat (int height, int width, int weight, boolean isBaby){
+  public Cat(int height, int width, int weight, boolean isBaby) {
     this.height = height;
     this.width = width;
     this.weight = weight;
     this.isBaby = isBaby;
   }
 
-  public void isSneaky(Cat cat){
+  public boolean isSneaky() {
 
-    if (cat.height <= 50 && cat.width <= 30 && !cat.isBaby){
-      isSneaky = true;
-    }
-    else if (cat.weight <= 20 && !cat.isBaby){
-      isSneaky = true;
-    }
-    else if (cat.isBaby){
-      isSneaky = true;
-    }else isSneaky = false;
+    if (height <= 50 && width <= 30 && !isBaby) {
+      return true;
+    } else if (weight <= 20 && !isBaby) {
+      return true;
+    } else if (isBaby) {
+      return true;
+    } else return false;
   }
 
-  public void isCatSneaky(Cat cat){
-    isSneaky(cat);
-    if (isSneaky){
+  public void isCatSneaky() {
+    isSneaky();
+    if (isSneaky()) {
       for (int i = 0; i < 100; i++) {
         System.out.println(i);
       }
-    }
-    else System.out.println("Not Sneaky!");
+    } else System.out.println("Not Sneaky!");
   }
 }
--- Miav/Miav/src/Main.java
+++ Miav/Miav/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    Cat cat = new Cat(35,55,4,false);
-  Cat cat1 = new Cat(20,15,2,true);
-  Cat cat2 = new Cat(10,10,2,false);
+    Cat cat = new Cat(35, 55, 25, false);
+    Cat cat1 = new Cat(20, 15, 2, true);
+    Cat cat2 = new Cat(10, 10, 2, false);
 
-    cat.isCatSneaky(cat);
-    cat1.isCatSneaky(cat1);
-    cat2.isCatSneaky(cat2);
+    cat.isCatSneaky();
+    cat1.isCatSneaky();
+    cat2.isCatSneaky();
   }
 }
