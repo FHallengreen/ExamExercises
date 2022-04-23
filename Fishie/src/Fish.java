4b7d65105e22e8424693977e7cfbfafaee1713fa
FHallengreen <fred70p9@stud.kea.dk>
2022-04-23 13:28:10 +0200

    new version

--- Fishie/src/Fish.java
+++ Fishie/src/Fish.java
@@ -28,20 +28,17 @@ public class Fish {
 
 
   public boolean foodChecker() {
-    int countC = 0;
-    int countA = 0;
     for (int i = 0; i < fishyLikes.size(); i++) {
       String firstFish = fishyLikes.get(i);
       for (int j = 0; j < firstFish.length(); j++) {
-        if (firstFish.charAt(i) == c) {
+        if (firstFish.indexOf(i) == c) {
           countC++;
-        } else if (firstFish.charAt(i) == a) {
+        } else if (firstFish.indexOf(i) == a) {
           countA++;
         }
-
-        if ((countC >= 1) || (countA >= 3)) {
-          return false;
-        }
+      }
+      if ((countC >= 1) || (countA >= 3)) {
+        return false;
       }
       countA = 0;
       countC = 0;
